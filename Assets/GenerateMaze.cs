8c5f67954d673e054facc2aa37e6fea6eddac395
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2022-12-12 18:22:59 -0600

    Scaling WIP

--- Assets/GenerateMaze.cs
+++ Assets/GenerateMaze.cs
@@ -6,20 +6,51 @@ using UnityEngine;
 
 public class GenerateMaze : MonoBehaviour
 {
-    public GameObject cellPrefab;
+    public GameObject wallPrefab;
     public GameObject debugCube;
-    public int mazeSize = 10;
+    public int mazeSize = 22;
     public float wallY = 1.5f;
     private HashSet<Vector3> visitedCells = new HashSet<Vector3>();
 
-    private int[] xOffset = {0, 2, -2, 0};
-    private int[] zOffset = {2, 0, 0, -2};
+    private int recursiveCounter = 0;
+    private float cellWidth; //Width runs on the x-axis
+    private float cellLength; // Length runs on the z-axis
+    private float[] xOffset = { 0, 2, -2, 0 };
+    private float[] zOffset = { 2, 0, 0, -2 };
 
     private void Start()
     {
+        //define the cell width and length
+        cellWidth = wallPrefab.transform.localScale.x;
+        cellLength = wallPrefab.transform.localScale.z;
+
+        Debug.Log("width and length is " + cellWidth + " " + cellLength);
+
         generateGrid();
-        //removeWallAt(new Vector3(6, 10, 5));
-        testRandomUnivisitedNeighbor();
+        processCell(new Vector3(mazeSize/2, wallY, mazeSize / 2));
+        //createExit();
+    }
+
+    private void processCell(Vector3 cell) {
+        //Implement the Randomized depth-first search algorithm
+        recursiveCounter++;
+        visitedCells.Add(cell);
+        Vector3? nextStepLocation = getRandomUnvisitedNeighbor(cell);
+        while(nextStepLocation != null) {
+            Vector3 wallPosition = getWallBetween(cell, (Vector3) nextStepLocation);
+            removeWallAt(wallPosition);
+            if (recursiveCounter < 1000) {
+                processCell((Vector3)nextStepLocation);
+            }
+            nextStepLocation = getRandomUnvisitedNeighbor(cell);
+        }
+    }
+
+    private void createExit() {
+        //Removing the topright corner from the maze
+        removeWallAt(new Vector3(mazeSize, wallY, mazeSize));
+        removeWallAt(new Vector3(mazeSize - 1, wallY, mazeSize));
+        removeWallAt(new Vector3(mazeSize, wallY, mazeSize - 1));
     }
 
     private void testRandomUnivisitedNeighbor()
@@ -67,12 +98,12 @@ public class GenerateMaze : MonoBehaviour
         {
             for (int i = 0; i <= mazeSize; i++)
             {
-                Vector3 horizontalWallLocation = new Vector3(i, wallY, j);
-                Instantiate(cellPrefab, horizontalWallLocation, Quaternion.identity, transform);
+                Vector3 horizontalWallLocation = new Vector3((float) i * cellWidth, wallY, (float) j * cellLength);
+                Instantiate(wallPrefab, horizontalWallLocation, Quaternion.identity, transform);
                 if (i % 2 == 1)
                 {
-                    Vector3 verticalWallLocation = new Vector3(j, wallY, i);
-                    Instantiate(cellPrefab, verticalWallLocation, Quaternion.identity, transform);
+                    Vector3 verticalWallLocation = new Vector3((float) j * cellLength, wallY, (float) i * cellWidth);
+                    Instantiate(wallPrefab, verticalWallLocation, Quaternion.identity, transform);
                 }
             }
         }
@@ -123,17 +154,24 @@ public class GenerateMaze : MonoBehaviour
 
     private bool cellIsInMazeArea(Vector3 cell)
     {
-        bool validX = cell.x > 0 && cell.x < mazeSize;
-        bool validZ = cell.z > 0 && cell.z < mazeSize;
+        bool validX = cell.x > 0 && cell.x < mazeSize * cellWidth;
+        bool validZ = cell.z > 0 && cell.z < mazeSize * cellLength;
         bool validY = cell.y == wallY;
         return validX && validY && validZ;
+    }
+
+    private Vector3 getWallBetween(Vector3 currentCell, Vector3 neighborCell) {
+        Vector3 deltaVector = neighborCell - currentCell;
+        deltaVector /= 2;
+        Vector3 wallPosition = currentCell + deltaVector;
+        return wallPosition;
     }
 
     private void removeWallAt(Vector3 cell)
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(cell, Vector3.down, out hit))
+        if (Physics.Raycast(cell + new Vector3(0, 10, 0), Vector3.down, out hit))
         {
             print("found an object - distance: " + hit.distance);
             print("found an object - objectname: " + hit.collider.gameObject);
